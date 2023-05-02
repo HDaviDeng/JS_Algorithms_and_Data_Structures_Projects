51c3999baba73dd6f3e95e9e94d87d6ad013afd6
Ririhanna <112195420+Ririhanna@users.noreply.github.com>
2023-05-02 09:24:46 -0400

    solution for charCode

--- ceasarsCipher.js
+++ ceasarsCipher.js
@@ -9,7 +9,15 @@
  */
 
 function rot13(str) {
-    return str;
+    let decoded = '';
+    for (let i = 0; i < str.length; i++) {
+        let charCode = str.charCodeAt(i);
+        if (charCode >= 65 && charCode <= 90) {
+            charCode = ((charCode - 65 + 13) % 26) + 65;
+        }
+        decoded += String.fromCharCode(charCode);
+    }
+    return decoded;
   }
   
   rot13("SERR PBQR PNZC");
