00c297dbfb9b49531baaccd26aed3415b2604ccb
Ririhanna <112195420+Ririhanna@users.noreply.github.com>
2023-05-02 09:00:56 -0400

    solution for romanNumeralConverter

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -20,7 +20,29 @@
  */
 
 function convertToRoman(num) {
-    return num;
+    const romanNumeralMap = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    let result = '';
+    for (let i in romanNumeralMap) {
+        while (num >= romanNumeralMap[i]) {
+            result += i;
+            num -= romanNumeralMap[i];
+        }
+    }
+    return result;
    }
    
    convertToRoman(36);
