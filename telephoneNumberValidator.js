b578601480e32ecee305b66a77ac93d1fe8a7679
Ririhanna <112195420+Ririhanna@users.noreply.github.com>
2023-05-02 11:05:48 -0400

    。。

--- telephoneNumberValidator.js
+++ telephoneNumberValidator.js
@@ -14,7 +14,9 @@
  */
 
 function telephoneCheck(str) {
-    return true;
+    // Define a regular expression pattern for valid US phone numbers
+    const pattern = /^(1\s?)?(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/;
+    return pattern.test(str);
   }
   
   telephoneCheck("555-555-5555");
