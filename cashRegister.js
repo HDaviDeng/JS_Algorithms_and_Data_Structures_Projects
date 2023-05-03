7c5b69d40ec70439a2fd93a99c3b9e77b7d44f61
Ririhanna <112195420+Ririhanna@users.noreply.github.com>
2023-05-03 09:32:18 -0400

    updated the last challenge

--- cashRegister.js
+++ cashRegister.js
@@ -42,3 +42,62 @@ function checkCashRegister(price, cash, cid) {
   }
   
   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+
+  const currencyUnitValues = {
+    "PENNY": 0.01,
+    "NICKEL": 0.05,
+    "DIME": 0.1,
+    "QUARTER": 0.25,
+    "ONE": 1,
+    "FIVE": 5,
+    "TEN": 10,
+    "TWENTY": 20,
+    "ONE HUNDRED": 100
+  };
+  
+  function checkCashRegister(price, cash, cid) {
+    const changeDue = cash - price;
+    const totalCID = cid.reduce((acc, curr) => {
+      return acc + curr[1];
+    }, 0);
+    if (totalCID < changeDue) {
+      return {status: "INSUFFICIENT_FUNDS", change: []};
+    } else if (totalCID === changeDue) {
+      return {status: "CLOSED", change: cid};
+    } else {
+      const change = [];
+      let remainingChangeDue = changeDue;
+      cid = cid.reverse();
+      for (let i = 0; i < cid.length; i++) {
+        const currencyUnit = cid[i][0];
+        const currencyUnitValue = currencyUnitValues[currencyUnit];
+        let availableCurrencyUnitAmount = cid[i][1];
+        let neededCurrencyUnitAmount = 0;
+        while (remainingChangeDue >= currencyUnitValue && availableCurrencyUnitAmount > 0) {
+          neededCurrencyUnitAmount += currencyUnitValue;
+          remainingChangeDue -= currencyUnitValue;
+          availableCurrencyUnitAmount -= currencyUnitValue;
+          remainingChangeDue = Math.round(remainingChangeDue * 100) / 100; // avoid rounding errors
+        }
+        if (neededCurrencyUnitAmount > 0) {
+          change.push([currencyUnit, neededCurrencyUnitAmount]);
+        }
+      }
+      if (remainingChangeDue > 0) {
+        return {status: "INSUFFICIENT_FUNDS", change: []};
+      }
+      return {status: "OPEN", change: change};
+    }
+  }
+  
+
+/**
+ * The 'currencyUnitValues' object maps each currency unit to its value. The 'checkCashRegister' function takes in the purchase price, payment amount, and cash-in- drawer as arguments.
+ * 
+ * It calculates the change due and the total amount of cash in the drawer. If the cash in the drawer is less than the change due, it returns'{status: "INSUFFICIEMT_FUNDS", change: []}'. If the cash in the drawer equals the change due, it returns '{status: "CLOSED", change: cid}' where 'cid' is the original cash-on-drawer array.
+ * 
+ * Otherwise, it goes through the cash-in-drawer array from highest to lowest value and calculates the amount of each currency unit needed to make up the change due. If there isn't enough of a particular currency unit, it moves on to the next one. If there isn't enough of a particular currency unit, it moves on to the next one. If there isn't enough cash in the drawer to make up the change due, it returns {status: "INSUFFICIENT_FUNDS", change: []}. Otherwise, it returns {status: "OPEN", change: change} where change is an array of the currency units and amounts needed to make up the change due.
+ */
+
+ 
